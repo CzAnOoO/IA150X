cb6ec774136cb4a58c71ec498d9139461f3c6784
Zhengan Chen <zhenganc@kth.se>
2026-03-23 16:14:06 +0100

    Test cropping

--- src/test.py
+++ src/test.py
@@ -3,40 +3,53 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy import ndimage
+from PIL import Image, ImageOps
+
 """ 
-Read .mat files in Python: https://codemia.io/knowledge-hub/path/read_mat_files_in_python
 https://docs.h5py.org/en/3.5.0/quick.html
  """
 with h5py.File("data/brainTumorDataPublic_1766/1.mat", "r") as f:
+    """ Read .mat files in Python: https://codemia.io/knowledge-hub/path/read_mat_files_in_python """
     print(list(f.keys()))
 
     cjdata = f["cjdata"]
     print(list(cjdata.keys()))
 
-    # img_ref = cjdata["image"]
-    # mask_ref = cjdata["tumorMask"]
-    # label_ref = cjdata["label"]
-
-    # img = np.array(f[img_ref])
-    # mask = np.array(f[mask_ref])
-    # label = int(np.array(f[label_ref]))
-
     """ https://medium.com/@gopiprasanthpotipireddy/using-mat-files-in-python-77293f995a43 """
     img = np.array(f["/cjdata/image"])
     PID = np.array(f["/cjdata/PID"])
-    label = int(np.array(f["/cjdata/label"]))
+    label = np.array(f["/cjdata/label"]).item()
     tumorBorder = np.array(f["/cjdata/tumorBorder"])
     tumorMask = np.array(f["/cjdata/tumorMask"])
 
     img = img.T
     tumorMask = tumorMask.T
+    iMg = img * tumorMask
 
-    plt.subplot(1, 2, 1)
+    # np.savetxt("image_matrix.txt", img, fmt="%f")
+
+    plt.subplot(2, 2, 1)
     plt.title(f"Image label={label}")
     plt.imshow(img, cmap="gray")
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(2, 2, 2)
     plt.title("Mask")
     plt.imshow(tumorMask, cmap="gray")
+
+    plt.subplot(2, 2, 3)
+    plt.title("Image && Mask")
+    plt.imshow(iMg, cmap="gray")
+
+    # Cropping
+    labeled_array, num_features = ndimage.label(iMg > 0)
+
+    slice = ndimage.find_objects(labeled_array)
+    tumor = slice[0]
+    iMg_cropped = iMg[tumor]
+
+    plt.subplot(2, 2, 4)
+    plt.title("Cropping")
+    plt.imshow(iMg_cropped, cmap="gray")
 
     plt.show()
