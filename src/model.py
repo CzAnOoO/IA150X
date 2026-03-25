63516cbd95d00949bcce0991410ef17e78cc9f11
MickeR94 <mikrud@kth.se>
2026-03-25 16:50:10 +0100

    Implemented a skeleton code for a training loop

--- src/model.py
+++ src/model.py
@@ -1,9 +1,53 @@
 import torch
 from torch import nn
 
-import torchvision
+import torch
+from torchvision import datasets, transforms, models
 from torchvision.transforms import ToTensor
 
 import matplotlib.pyplot as plt
 
-print("PyTorch successfully installed")
+
+# Make device agnostic code. When we train on Colabs GPUs device will be cuda
+device = "cuda" if torch.cuda.is_available() else "cpu" 
+
+# Import the ResNet model, setup of loss function and the optimizers. Note that we use a pretrained model by using weights='DEFAULT'
+# If we want to compare untrained, we simply leave it blank: model = models.resnet18()
+model = models.resnet18(weights='DEFAULT')
+#print(model)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model.parameters(),
+                            lr=0.01)
+''' optimizer = torch.optim.Adam(params=model.parameters(),
+                            lr=0.01) '''
+
+# TODO: Setup of Muon optimizer. Not as simple as optimizer = torch.optim.Muon(params=model.parameters(), lr=0.01) as it need 2D-parameters
+
+
+# Training the model
+epochs = 100
+
+for epoch in range(epochs):
+    model.train()
+
+    # TODO: Figure out how to forward pass for ResNet
+
+    # loss = loss_fn(x,y) # <-- Needs an input and target. Figure out what x, y can be
+    # TODO: Write an accuracy funciton to see how the model improves on training data
+
+    optimizer.zero_grad()
+    # Once the loss can be calculated, we need backpropagation by using loss.backward()
+    optimizer.step()
+
+# Evaluating the model
+model.eval()
+
+# TODO: Forward pass
+
+# TODO: Calculate loss once we've figured out the paramameters to the loss function; test_loss = loss_fn(x, y)
+
+# TODO: Calculate accuracy; test_accuracy
+
+# Uncomment this once loss and accuracy functions are implemented
+'''if epoch % 10 == 0:
+      print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")'''
