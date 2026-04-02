46a190492ca5ba70c2aacf16dbf0c752a54826e6
MickeR94 <mikrud@kth.se>
2026-04-02 18:18:40 +0200

    Added RandomRotation(10) to see if that affects the overfitting

--- src/model.py
+++ src/model.py
@@ -42,7 +42,8 @@ def get_dataloaders(batch_size: int = 16): # changed from 64 to 32 to 16
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
             ),  # https://docs.pytorch.org/vision/main/models/generated/torchvision.models.resnet18.html
-            transforms.RandomHorizontalFlip(p=0.5)
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomRotation(10)
         ]
     )
 
