7b8b1aae99c2d5114c69e354eaaf67b452e37015
mikrud <mikrud@kth.se>
2026-04-20 20:07:07 +0200

    Increasing batch size from 32 to 64 for Benchmark 3

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ import seed
 # opt_in_use = "adam"
 # model, loss_fn, optimizer = get_model_opt_loss()
 # model, loss_fn, optimizers = m.get_model_opt_loss(opt_name=opt_in_use, pretrain=False) <-- moving this down to the loop to reset for each optimizer
-train_dataloader, test_dataloader, val_dataloader = m.get_dataloaders(batch_size=32) #adjusting 16, 32, 64, 128
+train_dataloader, test_dataloader, val_dataloader = m.get_dataloaders(batch_size=64) #adjusting 16, 32, 64, 128
 optimizer_used = ["sgd", "adam", "muon"]
 all_results = {}
 
